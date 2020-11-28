ae4d0ede5e4b1a8c955c4c95722b8bc7060543e6
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2020-11-28 14:52:45 +0200

    Split code into multiple modules
    Code now is split into multiple modules
    Rewritten some parts
    Renamed some variables and functions
    Added docs (for my self :P)
    Now you can't unmute manually
    Now mutes only when the five main prayers start (by default)

---
+++ get_times.py
@@ -0,0 +1,43 @@
+import datetime
+
+"""
+All time processing functions are here
+"""
+
+
+def get_time() -> datetime.time:
+    """
+    Get current time object than replace seconds and microseconds with zeros \n
+    return datetime.time object
+    """
+    time = datetime.datetime.now().time()
+    time = time.replace(second=0, microsecond=0).strftime('%I:%M %p')
+    time = datetime.datetime.strptime(time, '%I:%M %p').time()
+    return time
+
+
+def get_prayer_times(prayers_dict, ignore_extra=True) -> datetime.time:
+    """
+    prayers_dict: dict with all prayers you got from your api
+    It must include ["Fajr","Dhuhr","Asr","Maghrib","Isha"] case sensitive keys
+    Times must be in 24h form as a string.
+    Example dict :{'Asr':'02:45','Maghrib':'04:32'}\n
+    ignore_extra default=True:\n
+    True: Ignore any extra prayers in the prayers dict\n
+    False: Don't ignore anything in the prayers dict\n
+    """
+    prayers_times = {}
+
+    # Turn the strings dict into datetime.time dict
+    for prayer in prayers_dict:
+        if (prayer not in ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"] and
+           ignore_extra is True):
+            continue
+
+        prayer_time_24H = datetime.datetime.strptime(prayers_dict[prayer],
+                                                     '%H:%M')
+        prayer_time_str = prayer_time_24H.strftime('%I:%M %p')
+        prayer_time = datetime.datetime.strptime(prayer_time_str, '%I:%M %p')
+        prayers_times[prayer] = prayer_time.time()
+
+    return prayers_times
--- soundControls.py
+++ soundControls.py
@@ -1,7 +1,11 @@
 from pycaw.pycaw import AudioUtilities
+from time import sleep
 
 
 def unmute():
+    """
+    Detect every audio output session and unmute it!
+    """
     sessions = AudioUtilities.GetAllSessions()
     for session in sessions:
         interface = session.SimpleAudioVolume
@@ -9,7 +13,24 @@ def unmute():
 
 
 def mute():
+    """
+    Detect every audio output session and mute it!
+    """
     sessions = AudioUtilities.GetAllSessions()
     for session in sessions:
         interface = session.SimpleAudioVolume
         interface.SetMute(1, None)
+
+
+def mute_unmute(delay):
+    """
+    delay -> int: delay between mute and unmute\n
+    Detect every audio output session, mute it and after (delay) unmute it
+    Note: The while loop is to make sure you didn't unmute manually!
+    """
+    i = 0
+    while i < delay:
+        mute()
+        sleep(1)
+        i += 1
+    unmute()
