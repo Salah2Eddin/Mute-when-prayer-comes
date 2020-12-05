a02d71933d422d286831afc18d93f05719791335
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2020-12-05 05:30:15 +0200

    Try sending requests to the api multiple times
    If there was no respond from the api, it try sending 2 more requests than return an error to the user if none of them returned a response

--- main.py
+++ main.py
@@ -6,26 +6,35 @@ from time import sleep
 import requests
 import datetime
 import urllib3
-
-# current time variables
-today = datetime.datetime.now().date()
+import sys
 
 
 def send_request():
-    try:
-        loc_data = get_loc()
-        return prayersAPI_request(loc_data)
-    except (requests.exceptions.ConnectionError or
-            urllib3.exceptions.ReadTimeoutError):
-        print('No internet connection , Reconnect than run the scirpt')
-        sys.exit()
+    i = 0
+    while i < 3:
+        try:
+            loc_data = get_loc()
+            return prayersAPI_request(loc_data)
+        except (requests.exceptions.ConnectionError or
+                urllib3.exceptions.ReadTimeoutError):
+            if i == 2:
+                print('No internet connection , Reconnect than run the scirpt')
+                sys.exit()
+            else:
+                print('Failed to connect to the api.. try:{}'.format(i+1))
+                i += 1
+                continue
 
 
-if __name__ == "__main__":
+def main():
+    today = datetime.datetime.now().date()
+
     request = send_request()
     prayer_times = get_prayer_times(request['data']['timings'])
+
     print(f'Date: {today}')
     print(prayer_times)
+
     while True:
         if check_prayer_now(prayer_times):
             mute_unmute(300)
@@ -36,3 +45,7 @@ if __name__ == "__main__":
             print(f'Date: {today}')
             print(prayer_times)
         sleep(15)
+
+
+if __name__ == "__main__":
+    main()
