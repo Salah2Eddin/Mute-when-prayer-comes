4a40786bbccf5ac2cd07c1d59b5a1b360a358c84
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2020-12-07 19:07:55 +0200

    Simple GUI
    Added a simple gui for the script ( not tested yet, not sure if works as intended)
    Moved the console application script to 'console.py'

---
+++ console.py
@@ -0,0 +1,32 @@
+from soundControls import mute_unmute
+from get_times import get_prayer_times
+from api_requests import send_request
+from check import check_prayer_now, check_date
+from time import sleep
+import datetime
+
+
+def main():
+    today = None
+    prayer_times = {}
+    while True:
+        if check_prayer_now(prayer_times):
+            mute_unmute(300)
+        if not check_date(today):
+            today = datetime.datetime.now().date()
+            request = send_request()
+            prayer_times = get_prayer_times(request['data']['timings'])
+
+            # Printing Date and Prayer times for the user
+            print(''.join(['=' for i in range(20)]))
+            print(f'Date: {today}')
+            print(''.join(['-' for i in range(len(str(today))+6)]))
+            print('Prayer Times: ')
+            print(''.join(['-' for i in range(len('Prayer Times: '))]))
+            for i, j in prayer_times.items():
+                print('{}: {}'.format(i, str(j)))
+        sleep(15)
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -3,30 +3,85 @@ from get_times import get_prayer_times
 from api_requests import send_request
 from check import check_prayer_now, check_date
 from time import sleep
+import tkinter as tk
 import datetime
+today = None
+prayer_times = {}
 
 
-def main():
-    today = None
-    prayer_times = {}
-    while True:
-        if check_prayer_now(prayer_times):
-            mute_unmute(300)
-        if not check_date(today):
-            today = datetime.datetime.now().date()
-            request = send_request()
-            prayer_times = get_prayer_times(request['data']['timings'])
+class PrayersTable(tk.Frame):
+    def __init__(self, parent, prayers_dict):
+        tk.Frame.__init__(self, parent)
+        self.prayers_dict = prayers_dict
+        self.labels = [
+            (tk.Label(self, text=i), tk.Label(self, text=j))
+            for i, j in prayers_dict.items()]
+        for i in range(len(self.labels)):
+            label = self.labels[i][0]
+            label.grid(column=i, row=0)
+            label = self.labels[i][1]
+            label.grid(column=i, row=1)
 
-            # Printing Date and Prayer times for the user
-            print(''.join(['=' for i in range(20)]))
-            print(f'Date: {today}')
-            print(''.join(['-' for i in range(len(str(today))+6)]))
-            print('Prayer Times: ')
-            print(''.join(['-' for i in range(len('Prayer Times: '))]))
-            for i, j in prayer_times.items():
-                print('{}: {}'.format(i, str(j)))
-        sleep(15)
+    def set_dict(self, new_dict):
+        self.prayers_dict = new_dict
+        self.labels = [
+            (tk.Label(self, text=i), tk.Label(self, text=j))
+            for i, j in new_dict.items()]
+        for i in range(len(self.labels)):
+            label = self.labels[i][0]
+            label.grid(column=i, row=0)
+            label = self.labels[i][1]
+            label.grid(column=i, row=1)
 
 
-if __name__ == "__main__":
-    main()
+class TimeLabel(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.time = datetime.datetime.now().time()
+        self.time_str = self.time.strftime('%I:%M:%S %p')
+        self.time_label = tk.Label(self, text=self.time_str)
+        self.time_label.grid(column=0, row=0)
+        self.after(s2ms(1), self.set_time)
+
+    def set_time(self):
+        self.time = datetime.datetime.now().time()
+        self.time_str = self.time.strftime('%I:%M:%S %p')
+        self.time_label.configure(text=self.time_str)
+        self.time_label.grid(column=0, row=0)
+        self.after(s2ms(1), self.set_time)
+
+
+def s2ms(sec):
+    """
+    Turns Seconds into MicroSeconds
+    """
+    return sec*1000
+
+
+def check():
+    global today, prayer_times
+    if check_prayer_now(prayer_times):
+        mute_unmute(300)
+    if not check_date(today):
+        today = datetime.datetime.now().date()
+        request = send_request()
+        prayer_times = get_prayer_times(request['data']['timings'])
+        date.configure(text='Date: {}'.format(datetime.datetime.now().date()))
+        table.set_dict(prayer_times)
+        window.after(s2ms(15), check)
+
+
+window = tk.Tk()
+window.title("Mute when prayer comes")
+window.geometry('360x400')
+date = tk.Label(window, text='Date: {}'.format(''))
+date.grid(column=0, row=0)
+time = TimeLabel(window)
+time.grid(column=1, row=0)
+table = PrayersTable(window, {})
+table.grid(column=0, row=1)
+
+if __name__ == '__main__':
+    print('Started main loop')
+    window.after(1, check)
+    window.mainloop()
